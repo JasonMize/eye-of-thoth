b10e6766086b6ec05ae553a070a7f5365cc0cfcf
Jason Mize <jasonemize@gmail.com>
2016-09-12 14:11:48 -0400

    updated readme

--- combatants.py
+++ combatants.py
@@ -1,68 +1,68 @@
-import random
+# import random
 
-class Combatants(object):
+# class Combatants(object):
 
-	def __init__(self, player, monster, display):
-		self.player = player
-		self.monster = monster
-		self.display = display
+# 	def __init__(self, player, monster, display):
+# 		self.player = player
+# 		self.monster = monster
+# 		self.display = display
 
 
-	def physical_combat (self):
-		player_attack = random.randint(1, self.player.player_physical_attack)
-		# print("player physical attack: " + str(player_attack))
+# 	def physical_combat (self):
+# 		player_attack = random.randint(1, self.player.player_physical_attack)
+# 		# print("player physical attack: " + str(player_attack))
 
-		monster_attack = random.randint(1, self.monster.monster_physical_attack)
-		# print("monster physical attack: " + str(monster_attack))
+# 		monster_attack = random.randint(1, self.monster.monster_physical_attack)
+# 		# print("monster physical attack: " + str(monster_attack))
 
-		if self.display.location == "atrium_closet":
-			if player_attack <= monster_attack:		
-				self.player.health_adjustment(-1)
-				if self.player.is_alive:
-					self.display.location = "atrium_monster_fight_lose"	
+# 		if self.display.location == "atrium_closet":
+# 			if player_attack <= monster_attack:		
+# 				self.player.health_adjustment(-1)
+# 				if self.player.is_alive:
+# 					self.display.location = "atrium_monster_fight_lose"	
 
-			elif player_attack > monster_attack:
-				self.monster.monster_health_adjustment(-1)
-				if self.monster.is_alive:
-					self.display.location = "atrium_monster_fight_win"	
+# 			elif player_attack > monster_attack:
+# 				self.monster.monster_health_adjustment(-1)
+# 				if self.monster.is_alive:
+# 					self.display.location = "atrium_monster_fight_win"	
 				
-		elif (self.display.location == "atrium_fight_loop_physical_win" 
-			or self.display.location == "atrium_fight_loop_physical_lose"
-			or self.display.location == "atrium_monster_fight_lose"
-			or self.display.location == "atrium_monster_fight_win"
-			or self.display.location == "atrium_monster_flee"
-			):
+# 		elif (self.display.location == "atrium_fight_loop_physical_win" 
+# 			or self.display.location == "atrium_fight_loop_physical_lose"
+# 			or self.display.location == "atrium_monster_fight_lose"
+# 			or self.display.location == "atrium_monster_fight_win"
+# 			or self.display.location == "atrium_monster_flee"
+# 			):
 			
-			if player_attack <= monster_attack:
-				self.player.health_adjustment(-1)
-				self.display.location = "atrium_fight_loop_physical_lose"
+# 			if player_attack <= monster_attack:
+# 				self.player.health_adjustment(-1)
+# 				self.display.location = "atrium_fight_loop_physical_lose"
 
-			elif player_attack > monster_attack:
-				self.monster.monster_health_adjustment(-1)
-				if self.monster.is_alive:	
-					self.display.location = "atrium_fight_loop_physical_win"		
+# 			elif player_attack > monster_attack:
+# 				self.monster.monster_health_adjustment(-1)
+# 				if self.monster.is_alive:	
+# 					self.display.location = "atrium_fight_loop_physical_win"		
 					
 
-	def mystical_combat (self):
-		player_attack = random.randint(1, self.player.player_mystical_attack)
-		# print("player mystical attack: " + str(player_attack))
+# 	def mystical_combat (self):
+# 		player_attack = random.randint(1, self.player.player_mystical_attack)
+# 		# print("player mystical attack: " + str(player_attack))
 
-		monster_attack = random.randint(1, self.monster.monster_mystical_attack)
-		# print("monster mystical attack: " + str(monster_attack))
+# 		monster_attack = random.randint(1, self.monster.monster_mystical_attack)
+# 		# print("monster mystical attack: " + str(monster_attack))
 		
-		if (self.display.location == "atrium_fight_loop_mystical_win"
-			or self.display.location == "atrium_fight_loop_mystical_lose"
-			or self.display.location == "atrium_monster_flee"
-			):
+# 		if (self.display.location == "atrium_fight_loop_mystical_win"
+# 			or self.display.location == "atrium_fight_loop_mystical_lose"
+# 			or self.display.location == "atrium_monster_flee"
+# 			):
 
-			if player_attack <= monster_attack:
-				self.player.sanity_adjustment(-1)
-				self.display.location = "atrium_fight_loop_mystical_lose"
+# 			if player_attack <= monster_attack:
+# 				self.player.sanity_adjustment(-1)
+# 				self.display.location = "atrium_fight_loop_mystical_lose"
 			
-			elif player_attack > monster_attack:
-				self.monster.monster_sanity_adjustment(-1)
-				if self.monster.is_alive:	
-					self.display.location = "atrium_fight_loop_mystical_win"		
+# 			elif player_attack > monster_attack:
+# 				self.monster.monster_sanity_adjustment(-1)
+# 				if self.monster.is_alive:	
+# 					self.display.location = "atrium_fight_loop_mystical_win"		
 			
 
 
